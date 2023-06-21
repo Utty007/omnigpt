f337a7c583c57d76ddad42261ccd9ee33fd4a53a
Kayode Uthman <88583385+Utty007@users.noreply.github.com>
2023-06-21 03:12:37 -0700

    Made several modifications for the desktop view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Frame1 from "./Components/Main/Frame1/Frame1";
 import Frame2 from "./Components/Main/Frame2/Frame2";
 import Frame3 from "./Components/Main/Frame3/Frame3";
+import Frame4 from "./Components/Main/Frame4/Frame4";
+import Frame5 from "./Components/Main/Frame5/Frame5";
+import Frame6 from "./Components/Main/Frame6/Frame6";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Frame1 />
       <Frame2 />
       <Frame3 />
+      <Frame4 />
+      <Frame5 />
+      <Frame6 />
     </div>
   );
 }
---
+++ src/Components/Main/Frame6/Frame6.js
@@ -0,0 +1,8 @@
+import yellowStar from "../../../Images/yellowStar.png";
+import purpleStar from "../../../Images/purpleStar.png";
+
+const Frame6 = () => {
+  return <div></div>;
+};
+
+export default Frame6;
